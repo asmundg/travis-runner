0f8d5108e8132124beecb4ba4e2d59952a780309
Åsmund Grammeltvedt <asgramme@microsoft.com>
2017-02-26 14:05:53 +0100

    v0.6.0

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+# 0.6.0
+
+* Add dist config keyword, default is precise
+* Add git to the default toolset
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='travis-runner',
-      version='0.5.0',
+      version='0.6.0',
       description='Local job runner for travis using docker',
       url='https://github.com/asmundg/travis-runner',
       author='Åsmund Grammeltvedt',
